b3ffe3762557819b328b218a30c08abb90a34a9d
nicolasplovie <nicolas.plovie@gmail.com>
2018-03-12 10:10:26 +0100

    init project

---
+++ dist/routes/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const route_1 = require("./route");
+class IndexRoute extends route_1.BaseRoute {
+    static create(router) {
+        console.log("[IndexRoute::create] Creating index route.");
+        router.get("/", (req, res, next) => {
+            new IndexRoute().index(req, res, next);
+        });
+    }
+    constructor() {
+        super();
+    }
+    index(req, res, next) {
+        let options = {
+            "message": "Welcome to the Tour of Heros"
+        };
+        this.render(req, res, "index.pug", options);
+    }
+}
+exports.IndexRoute = IndexRoute;
